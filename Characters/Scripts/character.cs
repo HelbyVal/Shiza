b112b96f909d508c3e49a85714bbfe9df0367110
MKstii <101364255+MKstii@users.noreply.github.com>
2024-03-27 18:47:54 +0500

    Movement

--- Characters/Scripts/character.cs
+++ Characters/Scripts/character.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class character : CharacterBody2D
 {
+	private NavigationAgent2D navigationAgent;
 	public const float Speed = 300.0f;
 	Vector2 click_position = new Vector2();
 	Vector2 target_position = new Vector2();
@@ -10,23 +11,36 @@ public partial class character : CharacterBody2D
     public override void _Ready()
     {
         click_position = Position;
+		navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		navigationAgent.PathDesiredDistance = 4;
+		navigationAgent.TargetDesiredDistance = 4;
+
+		Callable.From(ActorSetup).CallDeferred();
     }
 
     public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		//Vector2 velocity = Velocity;
 
-		// Handle Jump.
 		if (Input.IsActionJustPressed("mouse_click")){
-			click_position = GetGlobalMousePosition();
+			var mousePosition = GetGlobalMousePosition();
+			navigationAgent.TargetPosition = mousePosition;
 		}
 
-		if(Position.DistanceTo(click_position) > 5){
-			target_position = (click_position - Position).Normalized();
-			velocity = target_position * Speed;
+		if(GlobalPosition.DistanceTo(navigationAgent.TargetPosition) > 5){
+			Vector2 nextPathPosition = navigationAgent.GetNextPathPosition();
+			var velocity = (nextPathPosition - GlobalPosition).Normalized() * Speed;
 			Velocity = velocity;
 			MoveAndSlide();
 		}
-		
 	}
+
+	private async void ActorSetup()
+    {
+        // Wait for the first physics frame so the NavigationServer can sync.
+        await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+
+        // Now that the navigation map is no longer empty, set the movement target.
+        navigationAgent.TargetPosition = GlobalPosition;
+    }
 }
