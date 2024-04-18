1171e52504bd5b2781a9a47ab0659ceca15e488b
MKstii <101364255+MKstii@users.noreply.github.com>
2024-04-18 17:11:29 +0500

    3d main gameplay

--- Scenes/3d/Player3D.cs
+++ Scenes/3d/Player3D.cs
@@ -3,13 +3,44 @@ using System;
 
 public partial class Player3D : CharacterBody3D
 {
+
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
+	private float mouseSens;
+	private Camera3D camera;
+	private Camera3D mainCamera;
 
+	private float cameraLimitUp = Godot.Mathf.DegToRad(60);
+	private float cameraLimitDown = Godot.Mathf.DegToRad(-60);
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+        camera = GetNode<Camera3D>("Camera3D");
+		camera.Current = true;
+		var parent = GetParent();
+		mainCamera = parent.GetNode<Camera3D>("MainCamera");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		var Global = GetNode<Global>("/root/Global");
+		mouseSens = Global.MouseSens;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+			if(@event is InputEventMouseMotion ev){
+				Rotation = new Vector3(0, this.Rotation.Y + (-ev.Relative.X * mouseSens), 0);
+				var cameraX = Mathf.Clamp(camera.Rotation.X + (-ev.Relative.Y * mouseSens), cameraLimitDown, cameraLimitUp);
+				camera.Rotation = new Vector3(cameraX, camera.Rotation.Y, 0);
+		}
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
 
@@ -21,9 +52,17 @@ public partial class Player3D : CharacterBody3D
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
+		if(Input.IsActionJustPressed("changeCamera")){
+			if(camera.Current){
+				mainCamera.Current = true;
+			}
+			else{
+				camera.Current = true;
+			}
+		}
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("moveRight", "moveLeft", "moveBack", "moveForward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
